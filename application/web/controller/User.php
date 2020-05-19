f550e577f5f0a6dbf4b418baf1db40cd94cb3125
matt <mpp21x@gmail.com>
2020-05-19 20:54:53 +0800

    新增 update user page, api

--- application/web/controller/User.php
+++ application/web/controller/User.php
@@ -11,7 +11,6 @@ use think\Request;
 use function filter_var;
 use function header;
 use function is_string;
-use function var_dump;
 use function view;
 use const FILTER_VALIDATE_EMAIL;
 
@@ -67,6 +66,76 @@ class User extends Controller
         return $this->createUserView($email, $name, $result);
     }
 
+    public function edit()
+    {
+        $request = Request::instance();
+
+        if (!$request->get("id")) {
+            header('Location: /web/user/index');
+            die;
+        }
+        $user = Db::table("users")
+            ->where(["id" => $request->get("id")])
+            ->find();
+
+        if (empty($user)) {
+            header('Location: /web/user/index');
+            die;
+        }
+
+
+        return $this->updateUserView($user);
+    }
+
+    public function update()
+    {
+        $request = Request::instance();
+
+        $id = $request->post("id");
+        if (!$id) {
+            header('Location: /web/user/index');
+            die;
+        }
+        $user = Db::table("users")
+            ->where(["id" => $request->post("id")])
+            ->find();
+        if (!$user) {
+            header('Location: /web/user/index');
+            die;
+        }
+
+        $email = $request->post("email");
+        $name = $request->post("name");
+
+
+        if (empty($email) || !filter_var($request->post('email'), FILTER_VALIDATE_EMAIL)) {
+            return $this->updateUserView($user);
+        }
+
+        if (empty($name) || !is_string($name)) {
+            return $this->updateUserView($user);
+        }
+
+        $user = [
+            "email" => $email,
+            "name" => $name,
+            "updated_at" => (new DateTime())->format("Y-m-d H:i:s"),
+        ];
+        Db::table("users")
+            ->where(["id" => $id])
+            ->update($user);
+
+        $user = Db::table("users")
+            ->where(["id" => $request->post("id")])
+            ->find();
+
+        return $this->fetch('edit', [
+            "title" => "User Edit Page",
+            "user" => $user,
+            "success" => true,
+        ]);
+    }
+
     public function destory()
     {
         $request = Request::instance();
@@ -92,5 +161,15 @@ class User extends Controller
             'name' => $name,
             'success' => $result,
         ]);
+    }
+
+    public function updateUserView($user, $result = false)
+    {
+        return $this->fetch('edit', [
+            'title' => 'create user',
+            'user' => $user,
+            'success' => $result,
+        ]);
+
     }
 }
